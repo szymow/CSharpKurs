15053cd0ad5dea747b60bcb4db6893821041df20
WINDOWS-SKNH5I6\Szymon <221673@edu.p.lodz.pl>
2018-09-30 17:43:40 +0200

    Pierwszy program Hello. Tablica args.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Hello
 {
@@ -10,6 +7,8 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            foreach (var name in args)
+            Console.WriteLine("Hello, "+name);
         }
     }
 }
